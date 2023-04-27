d7227742b56711157d57e48c302b7dd87ee4817a
dhekdo <daae0924@naver.com>
2023-04-27 15:42:24 +0900

    2023.04.27_2

--- js/index.js
+++ js/index.js
@@ -1,36 +1,37 @@
+// 헤더 햄버거 메뉴 클릭 이벤트 
+$(function () {
+    $(".sub_menu").click(function () {
+        $("#aside").css({ "display": "block" });
+        $("#side_menu_area").animate({ 'right': '0%' }, 100);
+        $("#side_bg").show();
+        $(window).scrollTop(0);
+
+        $('#side_bg').on('scroll touchmove mousewheel', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
+        });
+    });
+
+    $(".close_btn").click(function () {
+        $("#side_menu_area").animate({ right: '-50rem' }, 100, function () {
+            $("#aside").css({ "display": "none" });
+        });
+        $("#side_bg").hide();
+    });
+});
+
+// 햄버거 메뉴 리스트 시작
+$(function () {
+    $(".scroll_list>li").click(function () {
+        $(".list_li").stop().slideUp();
+        $(this).find(".list_li").stop().slideToggle();
+    });
+});
+
+// 화살표 바뀌는 코드
+
 window.onload = function () {
-    // 헤더 햄버거 메뉴 클릭 이벤트 
-    $(function () {
-        $(".sub_menu").click(function () {
-            $("#aside").css({ "display": "block" });
-            $("#side_menu_area").animate({ 'right': '0%' }, 100);
-            $("#side_bg").show();
-            $(window).scrollTop(0);
-
-            $('#side_bg').on('scroll touchmove mousewheel', function (event) {
-                event.preventDefault();
-                event.stopPropagation();
-                return false;
-            });
-        });
-
-        $(".close_btn").click(function () {
-            $("#side_menu_area").animate({ right: '-50rem' }, 100, function () {
-                $("#aside").css({ "display": "none" });
-            });
-            $("#side_bg").hide();
-        });
-    });
-
-    // 햄버거 메뉴 리스트 시작
-    $(function () {
-        $(".scroll_list>li").click(function () {
-            $(".list_li").stop().slideUp();
-            $(this).find(".list_li").stop().slideToggle();
-        });
-    });
-
-    // 화살표 바뀌는 코드
     $(function () {
         $(".scroll_list>li>a").click(function () {
             if ($(this).children().attr('src') == 'img/down-arrow.svg') {
